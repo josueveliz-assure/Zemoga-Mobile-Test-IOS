0a5ad1fa1a4b8d5cdd1b0b22f6327d9e0678366a
Josue Veliz <josue.veliz@training.assuresoft.com>
2024-01-30 10:44:06 -0400

    Initial Commit

---
+++ Zemoga-Mobile-Test-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zemoga-Mobile-Test-IOS
+//
+//  Created by Josue Veliz on 30/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zemoga-Mobile-Test-IOS/Zemoga_Mobile_Test_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zemoga_Mobile_Test_IOSApp.swift
+//  Zemoga-Mobile-Test-IOS
+//
+//  Created by Josue Veliz on 30/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Zemoga_Mobile_Test_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
