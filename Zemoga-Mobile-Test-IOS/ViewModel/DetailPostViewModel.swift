d56af2cdc100a7bc6626220e932226dee018ae9a
Josue Veliz <josuedemetriovelizescobar@gmail.com>
2024-02-01 17:43:46 -0400

    api manager updated and view models added

---
+++ Zemoga-Mobile-Test-IOS/ViewModel/DetailPostViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailPostViewModel.swift
+//  Zemoga-Mobile-Test-IOS
+//
+//  Created by Josue Veliz on 1/2/24.
+//
+
+import Foundation
---
+++ Zemoga-Mobile-Test-IOS/ViewModel/PostListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostListViewModel.swift
+//  Zemoga-Mobile-Test-IOS
+//
+//  Created by Josue Veliz on 1/2/24.
+//
+
+import Foundation
