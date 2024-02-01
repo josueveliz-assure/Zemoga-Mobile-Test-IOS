9a37037155a154aa31f8092846b695cb1744e5bb
Josue Veliz <josuedemetriovelizescobar@gmail.com>
2024-02-01 17:44:56 -0400

    view models updated

--- Zemoga-Mobile-Test-IOS/ViewModel/PostListViewModel.swift
+++ Zemoga-Mobile-Test-IOS/ViewModel/PostListViewModel.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+@Observable final class PostListViewModel {
+    var isLoading: Bool = false
+    var posts: [Post] = Post.sample
+    
+    func getPosts(page: Int) async {
+        defer { isLoading = false }
+        do {
+            isLoading = true
+            
+            let duration = Duration(secondsComponent: 2, attosecondsComponent: 0)
+            try await Task.sleep(for: duration)
+            
+            ApiManager.shared.getPosts(page: page, limit: 10) { result in
+                switch result {
+                case .success(let posts):
+                    print("Fetched \(posts.count) posts.")
+                    self.posts = posts
+                case .failure(let error):
+                    print("Error fetching posts: \(error)")
+                }
+            }
+        } catch {
+            posts = []
+            print(error.localizedDescription)
+        }
+    }
+    
+    func populateData(page: Int) async {
+        do {
+            let duration = Duration(secondsComponent: 2, attosecondsComponent: 0)
+            try await Task.sleep(for: duration)
+            
+            ApiManager.shared.getPosts(page: page, limit: 10) { result in
+                switch result {
+                case .success(let posts):
+                    print("Fetched \(posts.count) posts.")
+                    self.posts.append(contentsOf: posts)
+                case .failure(let error):
+                    print("Error fetching posts: \(error)")
+                }
+            }
+        } catch {
+            posts = []
+            print(error.localizedDescription)
+        }
+    }
+}
